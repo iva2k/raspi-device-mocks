b4be7aba19124d8af6858046b3f027d9d3aea7bc
iva2k <iva2k@yahoo.com>
2019-12-26 14:00:13 -0800

    Packaging

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author='iva2k',
     author_email='iva2k@yahoo.com',
     license='GNU',
-    packages=['rpidevmocks'],
+    packages=['rpidevmocks.py'],
     install_requires=[
       'random',
       'time',
